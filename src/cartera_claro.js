d17b0d1bc9e3a7208148f5a58cd0a290399553a6
frxncismor <frxncismor@gmail.com>
2020-11-05 19:38:34 -0600

    Reto finalizado

---
+++ src/cartera_claro.js
@@ -0,0 +1,38 @@
+const cartera = () => {
+	var guardarEstrellas = 0;
+
+	const contarDinero = (dinero) => {
+		if (dinero > 100) {
+			guardarEstrellas++;
+		}
+		let elemento = document.querySelector('h2');
+		elemento.innerHTML = `Usted tiene ${guardarEstrellas} ⭐`;
+	};
+
+	return contarDinero;
+};
+
+let miCartera = cartera();
+
+function ingresarDinero() {
+	let dinero = document.getElementById('money').value;
+	miCartera(dinero);
+	limpiar();
+}
+
+function limpiar() {
+	let input = document.getElementById('money');
+	input.value = '';
+}
+
+function addBackground() {
+	let elemento = document.getElementById('button');
+	elemento.classList.add('shadow');
+	elemento.classList.remove('shadow-out');
+}
+
+function removeBackground() {
+	let elemento = document.getElementById('button');
+	elemento.classList.remove('shadow');
+	elemento.classList.add('shadow-out');
+}
